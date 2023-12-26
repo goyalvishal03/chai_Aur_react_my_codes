abd457276c6d51aa391add244174204ed60439a6
goyalvishal03 <bt20cse134@iiitn.ac.in>
2023-12-26 18:05:15 +0530

    2nd class

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,7 +1,15 @@
+import Chai from "./chai"
 function App() {
 
   return (
-    <h1> chai aur react |Vg</h1>
+    // <h1> chai aur react |Vg</h1>
+    <>
+    <div>
+    <Chai/>
+    <h1>chai aur react</h1>
+    <p>test</p>
+    </div>
+    </>
   )
 }
 
